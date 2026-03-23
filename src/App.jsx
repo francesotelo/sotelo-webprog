b8fcf15059badb802473dd1b5b32abd018e86c03
Mindaphil Francisca Sotelo <smindaphil@gmail.com>
2026-03-24 00:04:37 +0800

    Initial commit for lab-act1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: Mindaphil Francisca Sotelo<br />
+          Email: smindaphil@gmail.com<br />
+          Other Personal Info: <a href="https://github.com/francesotelo" target="_blank" rel="noopener noreferrer">GitHub Profile</a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
