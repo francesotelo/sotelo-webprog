75481ed2e6ae5cdd9def88eb7d93576c64f1c864
Mindaphil Francisca Sotelo <smindaphil@gmail.com>
2026-05-19 12:19:16 +0800

    Lab Activity 7

---
+++ sotelo-server/temp/controllers/userController.js
@@ -0,0 +1,82 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find().select('-password');
+    res.json({ users });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    if (!req.body.password) {
+      return res.status(400).json({ message: 'Password is required' });
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = await User.create({ ...req.body, password: hashedPassword });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    if (req.body.password) {
+      req.body.password = await bcrypt.hash(req.body.password, 10);
+    }
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    if (!user.isActive) {
+      return res.status(403).json({ message: 'Your account is inactive. Please contact support.' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    if (user.role === 'viewer') {
+      return res.status(403).json({ message: 'Viewers are not allowed to log in.' });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, email: user.email, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.json({ message: 'Login successful', token, role: user.role, firstName: user.firstName });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Must have curly braces here!
+module.exports = { getUsers, createUser, updateUser, deleteUser, loginUser };
